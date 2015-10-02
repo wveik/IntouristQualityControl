83fda5cadc0de0cb677e04cbb20c742ec6aca1b2
wveik <kataev_06@list.ru>
2015-10-02 19:50:31 +0400

    Допилил до версии 0.0.3

--- www/app/demo.js
+++ www/app/demo.js
@@ -2,14 +2,17 @@ var app = angular.module('mobileApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
 
 app.config(function($routeProvider) {
   $routeProvider.when('/',						{templateUrl: 'views/main.html', reloadOnSearch: false});
-  $routeProvider.when('/index.html',			{templateUrl: 'views/index.html', reloadOnSearch: false});   
+  $routeProvider.when('/index.html',			{templateUrl: 'views/index.html', reloadOnSearch: false, star : true});   
   $routeProvider.when('/test.html', 			{templateUrl: 'views/test.html', reloadOnSearch: false}); 
   $routeProvider.when('/index_enter.html',		{templateUrl: 'views/index_enter.html', reloadOnSearch: false}); 
-  $routeProvider.when('/question_type0.html',	{templateUrl: 'views/question_type0.html', reloadOnSearch: false}); 
-  $routeProvider.when('/question_type1.html',	{templateUrl: 'views/question_type1.html', reloadOnSearch: false}); 
+  $routeProvider.when('/question_type0.html',	{templateUrl: 'views/question_type0.html', reloadOnSearch: false, star : true}); 
+  $routeProvider.when('/question_type1.html',	{templateUrl: 'views/question_type1.html', reloadOnSearch: false, star : true}); 
   $routeProvider.when('/load.html',				{templateUrl: 'views/load.html', reloadOnSearch: false}); 
   $routeProvider.when('/message.html',			{templateUrl: 'views/message.html', reloadOnSearch: false}); 
-  $routeProvider.when('/plane.html',			{templateUrl: 'views/plane.html', reloadOnSearch: false}); 
+  $routeProvider.when('/plane.html',            {templateUrl: 'views/plane.html', reloadOnSearch: false}); 
+  $routeProvider.when('/bonus.html',            {templateUrl: 'views/bonus.html', reloadOnSearch: false, pig : true}); 
+  $routeProvider.when('/doc.html',              {templateUrl: 'views/doc.html', reloadOnSearch: false, doc : true});
+  $routeProvider.when('/infotour.html',         {templateUrl: 'views/infotour.html', reloadOnSearch: false, info : true}); 
 });
 
 app.directive( 'backButton', function() {
@@ -28,13 +31,39 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
 
 	//$scope.path = 'http://localhost:61619/Mobile';
 	$scope.path = 'http://test-mobile.ntk-intourist.ru/Mobile';
-	$scope.my_email = "";
+	$scope.my_email = localStorage.login;
 	$scope.selected = null;
+    $scope.sum_bonus = 0;
 
-	/*$scope.$on("$routeChangeSuccess", function (scope, next, current) {
-        $scope.transitionState = "active"
-    });*/
-  
+    //Окраска нижнего меню
+    $scope.template_class_pig = "glyphicon glyphicon-piggy-bank";
+    $scope.class_pig = $scope.template_class_pig;
+
+    $scope.template_class_start = "glyphicon glyphicon glyphicon-star";
+    $scope.class_star = $scope.template_class_start;
+
+    $scope.template_class_doc = "glyphicon glyphicon glyphicon-list-alt";
+    $scope.class_doc = $scope.template_class_doc;
+
+    $scope.template_class_info = "glyphicon glyphicon glyphicon-briefcase";
+    $scope.class_info = $scope.template_class_info;
+
+	$scope.$on("$routeChangeSuccess", function (scope, next, current) {
+        $scope.class_pig = $scope.template_class_pig;
+        $scope.class_star = $scope.template_class_start;
+        $scope.class_doc = $scope.template_class_doc;
+        $scope.class_info = $scope.template_class_info;
+
+        if(next.$$route.pig)    $scope.class_pig += " g_b";
+        if(next.$$route.star)   $scope.class_star += " g_b";
+        if(next.$$route.doc)    $scope.class_doc += " g_b";
+        if(next.$$route.info)   $scope.class_info += " g_b";
+    });
+
+    $scope.SetMessageName = function(name){
+        $scope.message_name = name;
+    }
+
 	$scope.registrate = function (my_email) {
 
 		$http({
@@ -48,14 +77,14 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
         	if (result) {
         		alert('Вы зарегистрированы. Пароль на почте');
         	} else {
-        		alert('У вас неверен Email');
+        		alert('Данный e-mail уже зарегистрирован, нажмите ВОЙТИ');
         	}
         }).
         error(function (data, status, headers, config) {
-            alert('Используйте другой Email');
+            alert('Данный e-mail уже зарегистрирован, нажмите ВОЙТИ');
         });
 
-	}
+	}    
 
 	$scope.enter = function (login, password) { 
 		$location.path( "/load.html" );
@@ -70,17 +99,19 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
 	        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
         }).
         success(function (data, status, headers, config) {
+
         	var result = parseInt(data) == 1;
         	if (result) {
         		$scope.my_email = login;
-        		$location.path( "/index.html" );
+                $scope.GetTopicList();
+                localStorage.login = $scope.my_email;
         	} else {
-        		alert('У вас неверен Email или пароль');
+        		alert('Неверен EMAIL или пароль');
         		$window.history.back();
         	}        	
         }).
         error(function (data, status, headers, config) {
-            alert('У вас неверен Email или пароль');
+            alert('Неверен EMAIL или пароль');
             $location.path( "/index.html" );
         });
 	}
@@ -90,10 +121,14 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
 		$http({
 	        method  : "POST",
 	        url     : $scope.path + "/GetTopicList",
+            data    : $.param({
+                'mail': $scope.my_email
+            }),
 	        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
         }).
         success(function (data, status, headers, config) {
         	$scope.TopicList = data.Items;
+            $location.path( "/index.html" );
         }).
         error(function (data, status, headers, config) {
             console.log(data);
@@ -101,8 +136,8 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
 	}
 
 	$scope.GoToQuestion = function (item){
-		
-		$scope.selected = item;
+
+        $scope.selected = item;
 
 		if(parseInt($scope.selected.Topic.MTL_TYPE) == 0) {
 			$location.path( "/question_type0.html" );
@@ -112,6 +147,7 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
 	}
 
 	$scope.SendRating = function(){
+
 		$location.path( "/load.html" );
 
 		$http({
@@ -125,6 +161,8 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
         }).
         success(function (data, status, headers, config) {
         	$location.path( "/plane.html" );
+            $scope.selected.Topic.disabled = "true";
+
         }).
         error(function (data, status, headers, config) {
             $window.alert('Ошибка');
@@ -133,10 +171,31 @@ app.controller('MainCtrl', function ($scope, $http, $location, $window) {
         });
 	}
 
+    $scope.GetBonusSum = function(){
+
+        $location.path( "/load.html" );
+
+        $http({
+            method  : "POST",
+            url     : $scope.path + "/GetBonusSum",
+            data    : $.param({
+                'email'     : $scope.my_email
+            }),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+        }).
+        success(function (data, status, headers, config) {
+            $location.path( "/bonus.html" );
+            $scope.sum_bonus = data;
+        }).
+        error(function (data, status, headers, config) {
+            $window.alert('Ошибка');
+            // $location.path( "/index.html" );
+            $window.history.back();
+        });
+    }
+
 	$scope.SetEmailToLogin = function(my_email){
 		$scope.login = my_email;
 	}
-
-	$scope.GetTopicList();
   
 });
